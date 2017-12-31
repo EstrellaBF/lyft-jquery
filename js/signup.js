fbf4c8d9932b210f6d072dc2212e16cf5cc0b484
EstrellaBF <stern.von.nord>
2017-12-30 20:08:20 -0500

    se verifica que al hacer click en la bandera, se ve el codigo postal

--- js/signup.js
+++ js/signup.js
@@ -3,42 +3,62 @@ $(document).ready(function() {
   var indexCode;
   var rutaLocal = '../assets/images/';
   var arrImages = [
-    {url: 'peru.png'},
-    {url: 'chile.png'},
-    {url: 'mexico.png'},
-    {url: 'brazil.png'}
-  ];
-
-  //Codigo postal, se pondrá manual pero jalará de forma dinámica
-  var postalCode = [
-    {code: '+51'},
-    {code: '+56'},
-    {code: '+52'},
-    {code: '+55'}
+    {url: 'peru.png',
+    code:'+51'},
+    {url: 'chile.png',
+    code:'+56'},
+    {url: 'mexico.png',
+    code:'+52'},
+    {url: 'brazil.png',
+    code:'+55'}
   ];
   
+  // console.log(arrImages[0]);
+  // console.log(arrImages[0].url); //peru.png
+  // console.log(arrImages[0].code); //+51
 
-  console.log(arrImages);
-  console.log(arrImages[0].url); //peru.png
-  var cosha = arrImages[0].url;
-  console.log(postalCode[0].code); //+51
+  // console.log(arrImages);
+  // console.log(arrImages[0].url); //peru.png
+  // var cosha = arrImages[0].url;
   // console.log($flagIcon.eq(0).attr({'src': rutaLocal + arrImages[0].url, 'alt':arrImages[0].url}));
   // console.log(arrImages.length);
-  //Creando función en caso se quiera añadir más sedes y sea más dinámico
-  // indexCode = postalCode[i].code;
-  // for(var i = 0; i < arrImages.length; i++){
-  //   // $flagIcon.eq(i).attr({src:rutaLocal + arrImages[i].url, alt:arrImages[i].url});
-  //   $flagIcon.eq(i).attr('src', rutaLocal + arrImages[i].url);
-  // };
-  $.each(arrImages, function(index, val){
-    console.log(index);//3
-    console.log(val);//{url: "brazil.png"}
-    console.log(val.url);//brazil.png
+
+  //Creando función en caso se quiera añadir más banderas o paises y sea más dinámico, solo se tendría que añadir el id con el nombre del país a la sede y añadir al array postalcode
+  function flags(){
+    $.each(arrImages, function(index, val){
+      $flagIcon.eq(index).attr('src', rutaLocal + val.url);
+    });
+  };
+  flags();
+
+  //función para que jale los codigos y los ponga en el input
+  $flagIcon.on('click', function(e){  
+    console.log(e.target);
+    console.log(e.currentTarget);
+    console.log($(this).attr('src')); //../assets/images/peru.png
+    console.log($(this).attr('src')); //../assets/images/peru.png
+    console.log($('#postal-code-number'))
+    for(var i = 0; i < arrImages.length; i ++){
+      // debugger
+      if($(this).attr('src') === rutaLocal + arrImages[i].url){
+        $('#postal-code-number').text(arrImages[i].code);
+        
+      };
+    };
+    
   });
 
-  $flagIcon.on('click', function(e){
-    console.log(e.target);        //<img src="../assets/images/peru.png" class="img-responsive" alt="Responsive image">
-    console.log(e.currentTarget); //<a href="#" class="flag-icon" id="peru"><img src="../assets/images/peru.png" class="img-responsive" alt="Responsive image"></a>
-    console.log($('#peru').attr('src'));
-  });
+      
+      // console.log(val); //{url: "peru.png", code: "+51"}
+      // console.log(arrImages[index].url);//peru.png
+      // console.log($(this).attr('src'));
+      // console.log($('#peru').attr('src'));//../assets/images/peru.png
+      // console.log($flagIcon);
+      // if($(this).attr('src') === rutaLocal + arrImages[index].url){
+      //   console.log(true);
+      // } else {
+      //   console.log(false);
+      // };
+      
+  
 });
